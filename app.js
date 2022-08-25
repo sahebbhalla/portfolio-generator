6fb77cbf9d09286eba5d5c0d1cc7cc22381f8024
saheb <sahibbhalla.010@gmail.com>
2022-08-24 21:39:42 -0400

    Modularized the code and implemented user input

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const commandLineArgs = process.argv.slice(2,process.argv.length);
+const fs =require("fs");
+const generatePage = require('./src/page-tempalte.js');
 
 
-const printProfileData = (profileDataArr) =>{
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-      }
-    console.log("======")
-      profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-      });
-}
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const [name,github] =profileDataArgs;
+console.log(name,github);
 
-
-printProfileData(commandLineArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
